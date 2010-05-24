407d021bfc1a4ce4721ddb1f9248de2e3fe4c016
kingpig <kingpig@6cb40bc8-796e-c36d-f3f2-ffed0853fdaa>
2010-05-24 09:28:40 +0000

    First commit.

---
+++ LucidBeaver.pro
@@ -0,0 +1,8 @@
+SOURCES += main.cpp \
+    mainwindow.cpp \
+    glwidget.cpp \
+    paintWidget.cpp
+HEADERS += mainwindow.h \
+    glwidget.h \
+    paintwidget.h
+QT += opengl
---
+++ paintWidget.h
@@ -0,0 +1,17 @@
+#ifndef PAINTWIDGET_H
+#define PAINTWIDGET_H
+
+#include <QBrush>
+#include <QPen>
+#include <QPixmap>
+#include <QWidget>
+
+  class paintWidget : public QWidget
+  {
+      Q_OBJECT
+
+      paintWidget(QWidget *parent = 0);
+
+  };
+
+#endif // PAINTWIDGET_H
